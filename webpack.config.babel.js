1894af64ccf3dba1d2b9b2d5851c374e402ef7dc
José Manuel Lucas Muñoz <josemanuel@jmlweb.es>
2016-12-27 22:26:50 +0100

    Production environment by default for React

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,4 +1,14 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import webpack from 'webpack';
+
 export default {
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+      },
+    }),
+  ],
   output: {
     filename: 'client-bundle.js',
   },
